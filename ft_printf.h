ac844a4f35962d887c27fa139cbd703ecfcd8576
Ana Medina Burgos <anamedin@cbr1s5.42barcelona.com>
2024-02-23 13:05:40 +0100

    creacion de archivos

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 12:46:16 by anamedin          #+#    #+#             */
+/*   Updated: 2024/02/23 12:51:38 by anamedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+#endif
