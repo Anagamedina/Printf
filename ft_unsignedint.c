0a006ac154695d1e0557745da47ec6b42a5171fc
Ana Medina Burgos <anamedin@cbr1s5.42barcelona.com>
2024-04-20 11:29:13 +0200

    paco strict ok

--- ft_unsignedint.c
+++ ft_unsignedint.c
@@ -6,49 +6,41 @@
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:48:07 by anamedin          #+#    #+#             */
-/*   Updated: 2024/04/19 21:34:29 by anamedin         ###   ########.fr       */
+/*   Updated: 2024/04/20 11:27:37 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-unsigned int	ft_unsigned_rec(unsigned int n)
+static unsigned int	ft_unsigned_rec(unsigned int n)
 {
-	unsigned int	result;
-	unsigned int	write_return;
-
-	result = 0;
-	if (n >= 10)
-	{
-		write_return = ft_unsigned_rec(n / 10);
-		if (write_return == 1)
-			return (1);
-		result += write_return;
-	}
-	write_return = ft_putchar((n % 10) + '0');
-	if (write_return == 1)
-		return (1);
-	result += write_return;
-	return (result);
-}
-
-unsigned int ft_unsignedint( int n)
-{
-	unsigned int count;
+	int	count;
 	int write_return;
 
 	count = 0;
-    if (n < 0)
+	if (n >= 10)
 	{
-		n = -n; // Convert negative number to its positive equivalent
-		write_return = ft_putchar('-'); // Print '-' sign for negative numbers
-		if (write_return == 1)
-			return (1);
+		write_return = ft_unsigned_rec(n / 10);
+		if (write_return == -1)
+			return (-1);
 		count += write_return;
 	}
-	write_return = ft_unsigned_rec((unsigned int)n);
-    if (write_return == 1)
-        return (1);
-    count += write_return;
-    return (count);
+	write_return = ft_putchar(n % 10 + '0');
+	if (write_return == -1)
+		return (-1);
+	count += write_return;
+	return (count);
+}
+
+unsigned int ft_unsignedint(unsigned int n)
+{
+	int count;
+	int write_return;
+	
+	count = 0;
+	write_return = ft_unsigned_rec(n);
+	if (write_return == -1)
+		return (-1);
+	count += write_return;
+	return (count);
 }
